71202af909645d39d4c16eade5c8fd178bb3e46c
Mel Fabella <mf820am@gmail.com>
2020-11-30 13:57:26 -0800

    added associations

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,17 @@
 class Contact < ApplicationRecord
+  # Associations
+  # describing the relationship between models
+
+    # child model
+  # belongs_to
+    # parent model
+  # has_many
+  # has_one
+  # has_many :through
+  # has_one :through 
+  # depedent
+
+  has_many :notes, dependent: :destroy
+  # has_one :note
+  has_one :address, dependent: :destroy 
 end
